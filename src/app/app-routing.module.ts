84a9454af5c2a06ac845f6b5d419941425db30d7
Roberto Carvajal <robertocarvajalvelasco@gmail.com>
2022-10-22 16:08:00 +0200

    creado routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JugadoresComponent } from './components/jugadores/jugadores.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'jugadores', component: JugadoresComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
