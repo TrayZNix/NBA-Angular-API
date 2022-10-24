62b93bfa1f27571a8c9cdaf4903127a40d508ae1
JorgeTenorio96 <jorgetenorio96@gmail.com>
2022-10-24 20:52:19 +0200

    mat-cards que rellenen la página

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ImportsMaterialModule } from './modules/imports-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ImportsMaterialModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
