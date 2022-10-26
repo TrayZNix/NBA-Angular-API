598a7f7436bac6266859eed72f59561f704f0a96
JorgeTenorio96 <jorgetenorio96@gmail.com>
2022-10-26 13:47:00 +0200

    Realizado componetización de lista de equipos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ImportsMaterialModule } from './modules/imports-material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { HomepageComponent } from './components/homepage/homepage.component';
 import { HomepageModule } from './modules/homepage.module';
 import { FormsModule } from '@angular/forms';
+import { SingleTeamModule } from './modules/single-team.module';
 import { EquiposModule } from './modules/equipos.module';
-import { MaterialImportsModule } from './modules/material-imports.module';
-import { FooterComponent } from './components/footer/footer.component';
-import { FlexModule } from '@angular/flex-layout';
-import { JugadoresModule } from './modules/jugadores.module';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { FooterModule } from './modules/footer.module';
+import { JugadoresModule } from './modules/jugadores.module';
+import { MaterialImportsModule } from './modules/material-imports.module';
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
 
+
+
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
-    HomepageModule,
     MaterialImportsModule,
     FlexLayoutModule,
-    HttpClientModule,
+    HomepageModule,
     FormsModule,
+    SingleTeamModule,
     EquiposModule,
-    FlexModule,
-    JugadoresModule,
-    HttpClientModule,
-    ReactiveFormsModule,
     FooterModule,
+    HomepageModule,
+    JugadoresModule
+
+
+
+
+
+
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
